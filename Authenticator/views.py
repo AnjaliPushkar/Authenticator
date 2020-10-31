f75a9db8dbd85083be8336f03be2ed42a1adb88d
Anjali Pushkar <anjalipushkar15@gmail.com>
2020-11-01 02:15:48 +0530

    login page added

---
+++ Authenticator/views.py
@@ -0,0 +1,11 @@
+from django.contrib import auth
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
