1fc2f7c68d76ee899f102ef5905801ef02805f40
Anjali Pushkar <anjalipushkar15@gmail.com>
2020-11-29 05:43:55 +0530

    employee app added

--- Authenticator/urls.py
+++ Authenticator/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .import views
 from django.conf import settings
 
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.homepage, name = 'homepage'),
     path('login/', views.login, name = 'login'),
     path('signup/', views.signup, name = 'signup'),
+    path('company/', include('employee.urls')),
 ]
